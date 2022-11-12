6f589f9dbb9b5af32056ddc58e2ba55021970fe8
Harish Nigam <harishnigam21@gmail.com>
2022-11-12 17:25:45 +0530

    added css and  ui

---
+++ app/src/ui/sea-link.vue
@@ -0,0 +1,14 @@
+<template>
+  <sea-button role="link" v-on="$listeners" v-bind="$attrs">
+    <slot></slot>
+  </sea-button>
+</template>
+
+<script>
+import SeaButton from "./sea-button"
+
+export default {
+  name: "sea-link",
+  components: { SeaButton },
+}
+</script>
