7019db556519d63fc6494accd3fce2d9ee7b262b
yashsiwdiya <yashsiwdiya@gmail.com>
2022-11-15 23:27:10 +0530

    added sea-form-group

---
+++ app/src/ui/sea-form-group.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-group">
+    <label :for="uid" class="form-label label-sm" v-if="label">
+      {{ label }}
+    </label>
+    <div class="form-body">
+      <slot v-bind:uid="uid"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import { uuid } from "zeed"
+
+export default {
+  name: "sea-form-group",
+  props: {
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      uid: uuid(),
+    }
+  },
+}
+</script>
