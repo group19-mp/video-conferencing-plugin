f581279ab4bb3fb52db9102a0e83e40df2d57571
SonyGadhre <sonygadhre@gmail.com>
2022-11-16 23:53:18 +0530

    commited sea-symbol

---
+++ app/src/ui/sea-symbol.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <i class="f7" :data-f7-icon="name" aria-hidden="true" />
+</template>
+
+<script>
+export default {
+  name: "sea-symbol",
+  props: {
+    name: {
+      type: String,
+      default: 0,
+      required: true,
+    },
+  },
+}
+</script>
